f10e33bf8955ad335f4c60e5190a8d98c489210e
Gerard Allan <Gerard.Allan@2mee.com>
2022-11-07 11:03:01 +0000

    kitura3

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/Kitura/LoggerAPI.git", .upToNextMajor(from: "1.9.200")),
+    .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "2.0.0")),
   ],
   targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
